86ee6ad2d403b135b8f853cfdd23405c8103231a
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-12-04 15:48:14 -0300

    Conversions

---
+++ js/conversions.js
@@ -0,0 +1,19 @@
+// tipo de dado
+// booleanos
+
+// conversão implicita
+/*
+const numero = 456;
+const numeroString = Number("456");
+// const numeroString = Number("456a"); // NaN
+
+console.log(numero + numeroString);
+*/
+
+// conversao explicita
+// Number()
+// String()
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero + Number(numeroString));
