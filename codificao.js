2418c258d81037418bec8fde198767963e2d529e
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-12-04 10:33:49 -0300

    Atualização do README

--- codificao.js
+++
@@ -1,9 +0,0 @@
-const cifrao = '\u0024'
-const aMaiusculo = '\u0041'
-const tique = '\u2705'
-const hiragana = '\u3041'
-
-console.log(cifrao)
-console.log(aMaiusculo)
-console.log(tique)
-console.log(hiragana)
