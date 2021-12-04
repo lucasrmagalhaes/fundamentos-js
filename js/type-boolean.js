d9b50c37a124e3d2988802558457815bb66913e6
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-12-04 10:31:16 -0300

    Tipo boolean

---
+++ js/type-boolean.js
@@ -0,0 +1,9 @@
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+console.log(primeiroNumero === segundoNumero); // false
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2); // false
