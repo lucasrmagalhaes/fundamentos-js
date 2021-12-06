04842344037499f3e2f73291dd1d39be22437753
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-12-06 20:27:26 -0300

    Comparações

---
+++ js/comparisons.js
@@ -0,0 +1,14 @@
+// == (comparacao implicita)
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero === texto) // false
+
+// typeof
+
+console.log(typeof numero) // number
+console.log(typeof texto) // string
+
+// == compara apenas o valor
+// === compara valor e tipo de dado
