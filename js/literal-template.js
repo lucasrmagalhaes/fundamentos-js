629819a6b1b2497fbc3c4ea1959a0223ac9fcc8f
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-12-07 20:22:20 -0300

    Template literal

---
+++ js/literal-template.js
@@ -0,0 +1,8 @@
+const nome = "Lucas";
+const idade = 2021 - 1992;
+const cidadeNatal = "Porto Alegre"
+// const apresentacao = " meu nome é " + nome + ", tenho " + idade + " anos e nasci na cidade de " + cidadeNatal;
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeNatal}`;
+
+console.log(apresentacao)
